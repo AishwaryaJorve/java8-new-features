3e1014a4ab71f60f209309fba3621e62d1b4032d
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2021-05-27 13:33:09 +0530

    Lambda implementation

---
+++ src/main/java/com/aish/java8/lambdaExpression/Test1.java
@@ -0,0 +1,20 @@
+package com.aish.java8.lambdaExpression;
+
+interface Interf{
+//	public void m1();
+	
+	public void add(int a,int b);
+}
+
+
+public class Test1 {
+    public static void main(String args[]){
+      
+//    	Interf i=()->System.out.println("Lambda implementation");
+//    	i.m1();
+
+    	Interf i=(a,b)->System.out.println("sum byLambda implementation"+(a+b));
+    	i.add(10,20);
+    	
+    }
+}
