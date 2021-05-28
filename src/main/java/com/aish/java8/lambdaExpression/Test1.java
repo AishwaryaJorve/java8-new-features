e6276592048e49a8d9929d085b205a0fb7d3ca79
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2021-05-28 13:42:35 +0530

    example

--- src/main/java/com/aish/java8/lambdaExpression/Test1.java
+++ src/main/java/com/aish/java8/lambdaExpression/Test1.java
@@ -1,11 +1,14 @@
 package com.aish.java8.lambdaExpression;
 
 interface Interf{
+	
 //	public void m1();
 	
 //	public void add(int a,int b);
 	 
-	public int getLength(String s);
+//	public int getLength(String s);
+	
+	public int squareIt(int x);
 	
 }
 
@@ -19,8 +22,11 @@ public class Test1 {
 //    	Interf i=(a,b)->System.out.println("sum byLambda implementation"+(a+b));
 //    	i.add(10,20);
     	
-    	Interf i=(s)->s.length();
-    	System.out.println(i.getLength("Aishwarya"));
+//    	Interf i=(s)->s.length();
+//    	System.out.println(i.getLength("Aishwarya"));
+    	
+    	Interf i=x->x*x;
+    	System.out.println(i.squareIt(10));
     	
     }
 }
