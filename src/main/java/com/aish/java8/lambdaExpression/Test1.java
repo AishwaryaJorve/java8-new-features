ab90b08a349f600702f4c9ec9968ba36bf8b713b
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2021-05-28 13:28:40 +0530

    Invoke lambda expression

--- src/main/java/com/aish/java8/lambdaExpression/Test1.java
+++ src/main/java/com/aish/java8/lambdaExpression/Test1.java
@@ -3,7 +3,10 @@ package com.aish.java8.lambdaExpression;
 interface Interf{
 //	public void m1();
 	
-	public void add(int a,int b);
+//	public void add(int a,int b);
+	 
+	public int getLength(String s);
+	
 }
 
 
@@ -13,8 +16,11 @@ public class Test1 {
 //    	Interf i=()->System.out.println("Lambda implementation");
 //    	i.m1();
 
-    	Interf i=(a,b)->System.out.println("sum byLambda implementation"+(a+b));
-    	i.add(10,20);
+//    	Interf i=(a,b)->System.out.println("sum byLambda implementation"+(a+b));
+//    	i.add(10,20);
+    	
+    	Interf i=(s)->s.length();
+    	System.out.println(i.getLength("Aishwarya"));
     	
     }
 }
