85e9311fc758b09d89df2a8a48398570399f72e5
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2021-05-29 15:12:16 +0530

    TreeMap demo with lambda expression

---
+++ src/main/java/com/aish/java8/lambdaExpression/TreeMapDemoWithLambdaExpression.java
@@ -0,0 +1,20 @@
+package com.aish.java8.lambdaExpression;
+
+import java.util.TreeMap;
+
+public class TreeMapDemoWithLambdaExpression {
+
+	
+public static void main(String[] args) {
+		
+		// While creating object pass lambda expression 
+		TreeMap<Integer,String> t=new TreeMap<>((i1,i2)->(i1>i2)?-1:(i1<i2)?1:0);
+		t.put(1, "Durga");
+		t.put(6, "Aishwarya");
+		t.put(9, "Shubhangi");
+		t.put(100, "Mahesh");
+		System.out.println(t);
+	}
+	
+	
+}
--- src/main/java/com/aish/java8/lambdaExpression/TreeSetDemoWithLambdaExpression.java
+++ src/main/java/com/aish/java8/lambdaExpression/TreeSetDemoWithLambdaExpression.java
@@ -4,17 +4,17 @@ import java.util.TreeSet;
 
 public class TreeSetDemoWithLambdaExpression {
 
-	public static void main(String[] args) {
-		
-		// While creating object pass lambda expression 
-		TreeSet<Integer> t=new TreeSet<>((i1,i2)->(i1>i2)?-1:(i1<i2)?1:0);
-		t.add(12);
-		t.add(20);
-		t.add(21);
-		t.add(1);
-		t.add(23);
-		t.add(3);
-		System.out.println(t);
-	}
+//	public static void main(String[] args) {
+//		
+//		// While creating object pass lambda expression 
+//		TreeSet<Integer> t=new TreeSet<>((i1,i2)->(i1>i2)?-1:(i1<i2)?1:0);
+//		t.add(12);
+//		t.add(20);
+//		t.add(21);
+//		t.add(1);
+//		t.add(23);
+//		t.add(3);
+//		System.out.println(t);
+//	}
 	
 }
