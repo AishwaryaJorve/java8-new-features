206a560cc301b34e715023ed411e0a294dca0086
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2021-05-29 17:10:16 +0530

    Anonymous inner class Vs lambda expression

---
+++ src/main/java/com/aish/java8/lambdaExpression/AnonymousInnerVSLambdaExpression.java
@@ -0,0 +1,78 @@
+package com.aish.java8.lambdaExpression;
+
+// Anonymous inner class VS Lambda Expression with Runnable
+
+/**
+ * Using Anonymous Inner class
+ * @author AishwaryaJorve
+ *
+ */
+//public class AnonymousInnerVSLambdaExpression {
+//
+//	public static void main(String[] args) {
+//      //Anonymous inner class
+//		Runnable r = new Runnable() {
+//
+//			@Override
+//			public void run() {
+//				for (int i = 0; i < 10; i++) {
+//					System.out.println("child Thread");
+//				}
+//
+//			}
+//		};
+//
+//		Thread t = new Thread(r);
+//		t.start();
+//		for (int i = 0; i < 10; i++) {
+//			System.out.println("Main thread");
+//		}
+//	}
+//
+//}
+
+/**
+ * Using lambda expression
+ * @author AishwaryaJorve
+ *
+ */
+//public class AnonymousInnerVSLambdaExpression {
+//
+//	public static void main(String[] args) {
+//		Runnable r = () -> {
+//			for (int i = 0; i < 10; i++) {
+//				System.out.println("child Thread");
+//			}
+//
+//		};
+//
+//		Thread t = new Thread(r);
+//		t.start();
+//		for (int i = 0; i < 10; i++) {
+//			System.out.println("Main thread");
+//		}
+//	}
+//
+//}
+
+/**
+ * Using lamda expression (Another way)
+ * @author AishwaryaJorve
+ *
+ */
+public class AnonymousInnerVSLambdaExpression {
+
+	public static void main(String[] args) {
+		Thread t = new Thread(() -> {
+			for (int i = 0; i < 10; i++) {
+				System.out.println("child Thread");
+			}
+
+		});
+		t.start();
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Main thread");
+		}
+	}
+
+}
--- src/main/java/com/aish/java8/lambdaExpression/CustomClassSortingWithLamdaExpression.java
+++ src/main/java/com/aish/java8/lambdaExpression/CustomClassSortingWithLamdaExpression.java
@@ -22,17 +22,17 @@ class Employee {
 
 public class CustomClassSortingWithLamdaExpression {
 
-	public static void main(String[] args) {
-
-		ArrayList<Employee> l = new ArrayList<Employee>();
-		l.add(new Employee(12, "Aishi"));
-		l.add(new Employee(19, "Shubhi"));
-		l.add(new Employee(2, "Durga"));
-		l.add(new Employee(8, "Mhashya"));
-		System.out.println("before" + l);
-		// lambda expression
-		Collections.sort(l, (e1, e2) -> (e1.eNo > e2.eNo) ? -1 : (e1.eNo < e2.eNo) ? 1 : 0);
-		System.out.println("after" + l);
-	}
+//	public static void main(String[] args) {
+//
+//		ArrayList<Employee> l = new ArrayList<Employee>();
+//		l.add(new Employee(12, "Aishi"));
+//		l.add(new Employee(19, "Shubhi"));
+//		l.add(new Employee(2, "Durga"));
+//		l.add(new Employee(8, "Mhashya"));
+//		System.out.println("before" + l);
+//		// lambda expression
+//		Collections.sort(l, (e1, e2) -> (e1.eNo > e2.eNo) ? -1 : (e1.eNo < e2.eNo) ? 1 : 0);
+//		System.out.println("after" + l);
+//	}
 
 }
