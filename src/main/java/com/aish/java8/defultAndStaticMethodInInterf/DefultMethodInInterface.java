01258669589f35e63e95f09eb186253ede7d55e9
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2021-06-08 18:14:23 +0530

    Define and accessed default method in interface

---
+++ src/main/java/com/aish/java8/defultAndStaticMethodInInterf/DefultMethodInInterface.java
@@ -0,0 +1,27 @@
+package com.aish.java8.defultAndStaticMethodInInterf;
+
+
+/**
+ * Default method have already implementation can use same implementaion and can override  
+ * @author AishwaryaJorve
+ *
+ */
+
+interface Interf{
+	
+	//with 'default' keyword can define default method in interface
+	default void m1() {
+		System.out.println("I am Default method in interface");
+	}
+}
+
+
+public class DefultMethodInInterface implements Interf{
+
+	public static void main(String[] args) {
+		DefultMethodInInterface t=new DefultMethodInInterface();
+		
+		t.m1();
+	}
+	
+}
