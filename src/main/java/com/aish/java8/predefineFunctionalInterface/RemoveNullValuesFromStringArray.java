06f780750ce0ce26666dda4847f918182450b118
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2021-06-11 21:38:19 +0530

    Using Predicate .. Remove empty string and null from array

---
+++ src/main/java/com/aish/java8/predefineFunctionalInterface/RemoveNullValuesFromStringArray.java
@@ -0,0 +1,27 @@
+package com.aish.java8.predefineFunctionalInterface;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+/**
+ * Remove null from array
+ * @author Aishwarya Jorve
+ */
+public class RemoveNullValuesFromStringArray {
+    public static void main(String[] args) {
+        String[] names={"Durga", "",null,"Ravi","Shivam","", null};
+
+        Predicate<String> p=s->s!=null && s.length()!=0;
+
+        ArrayList<String> l=new ArrayList<>();
+
+        for(String s:names){
+            if(p.test(s)){
+                l.add(s);
+            }
+        }
+
+        System.out.println("The list of valid Strings");
+        System.out.println(l);
+    }
+}
