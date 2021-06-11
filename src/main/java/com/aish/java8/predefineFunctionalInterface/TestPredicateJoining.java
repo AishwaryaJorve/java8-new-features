95e6c912e873f205294365fcfd1837b212455e99
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2021-06-11 17:53:16 +0530

    Predicate joining with example

--- src/main/java/com/aish/java8/predefineFunctionalInterface/TestPredicateJoining.java
+++ src/main/java/com/aish/java8/predefineFunctionalInterface/TestPredicateJoining.java
@@ -14,13 +14,32 @@ public class TestPredicateJoining {
 		Predicate<Integer> p2=i->i%2==0; //should be Even
 
 		System.out.println("the number greater than 10 are: ");
+		m1(p1,x);
+
+		System.out.println("Even numbers are: ");
+		m1(p2,x);
+
+		System.out.println("the number greater than 10 and Even are are: ");
+		m1(p1.and(p2),x);
+
+		System.out.println("the number not greater than 10 are: ");
+		m1(p1.negate(),x);
+
+		System.out.println("the number  greater than 10 otherwise Even are: ");
+		m1(p1.or(p2),x);
 
 	}
 	
 	// method to iterate array 
 	
 	public static void m1(Predicate<Integer> p,int[] x) {
-		
+
+		 for(int x1:x){
+		 	if(p.test(x1)){
+				System.out.println(x1);
+			}
+		 }
+
 	}
 	
 }
