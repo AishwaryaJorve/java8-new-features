0a94c675feeeee0d111dadc70a2f409b70f1fa0d
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2021-06-12 16:41:41 +0530

    Function interface with string length

---
+++ src/main/java/com/aish/java8/prdefineFunctinalInterfaceFunction/FindLengthOfString.java
@@ -0,0 +1,13 @@
+package com.aish.java8.prdefineFunctinalInterfaceFunction;
+
+import java.util.function.Function;
+
+public class FindLengthOfString {
+
+    public static void main(String[] args) {
+        Function<String,Integer> f=s->s.length();
+
+        System.out.println(f.apply("Aishwarya"));
+        System.out.println(f.apply("Monali"));
+    }
+}
