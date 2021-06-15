62d0c99a9776c9abb6575c81635d62b0f9d65910
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2021-06-15 23:33:24 +0530

    Somthing about Function interface

--- src/main/java/com/aish/java8/prdefineFunctinalInterfaceFunction/FindLengthOfString.java
+++ src/main/java/com/aish/java8/prdefineFunctinalInterfaceFunction/FindLengthOfString.java
@@ -1,6 +1,10 @@
 package com.aish.java8.prdefineFunctinalInterfaceFunction;
 
 import java.util.function.Function;
+/**
+ * Function interface is work like Predicate interface but Function interface return any type value
+ * Function interface take two types one is input parameter and another is Return type
+ */
 
 /**
  * Find length of string using Function interface
