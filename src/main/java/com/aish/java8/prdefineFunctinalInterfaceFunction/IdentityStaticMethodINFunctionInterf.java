6460dad31e5dd94dcd0aee428c45fd40093f8d14
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2021-06-19 21:46:06 +0530

    Static method identity()

---
+++ src/main/java/com/aish/java8/prdefineFunctinalInterfaceFunction/IdentityStaticMethodINFunctionInterf.java
@@ -0,0 +1,16 @@
+package com.aish.java8.prdefineFunctinalInterfaceFunction;
+
+import java.util.function.Function;
+
+/**
+ * Function interface contains a static method.
+ * static <T> Function<T,T> identity()
+ * Returns a function that always returns its input argument.
+ */
+public class IdentityStaticMethodINFunctionInterf {
+    public static void main(String[] args) {
+        Function<String, String> f1 = Function.identity();
+        String s2 = f1.apply("durga");
+        System.out.println(s2);
+    }
+}
