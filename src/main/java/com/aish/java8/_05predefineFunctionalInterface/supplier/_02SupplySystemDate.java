204b1198fb87b6ea01be2d3b91ce556ad96a9279
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2021-06-22 19:04:50 +0530

    Supply date and random otp using supplier Functional Interface

--- src/main/java/com/aish/java8/_05predefineFunctionalInterface/supplier/_02SupplySystemDate.java
+++ src/main/java/com/aish/java8/_05predefineFunctionalInterface/supplier/_02SupplySystemDate.java
@@ -2,6 +2,7 @@ package com.aish.java8._05predefineFunctionalInterface.supplier;
 
 /*
  * Supplier to supply system date
+ * Random OTP
  */
 
 import java.util.Date;
@@ -12,7 +13,20 @@ import java.util.function.Supplier;
  */
 public class _02SupplySystemDate {
     public static void main(String[] args) {
+
+        // Supplier for otp
+        Supplier<String> otps= () -> {
+            String otp = "";
+            for (int i = 1; i <= 6; i++) {
+                otp = otp + (int) (Math.random() * 10);
+            }
+            return otp;
+        };
+
+        // Supplier for date
         Supplier<Date> date = () -> new Date();
+
         System.out.println(date.get());
+        System.out.println(otps.get());
+        }
     }
-}
