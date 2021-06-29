b03a8b8e10258fdd013c46d477411582a85db74f
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2021-06-29 21:01:46 +0530

    IntSupplier basic with op

---
+++ src/main/java/com/aish/java8/_07primitiveTypeFunctionalInterfaces/IntSupplierDemo.java
@@ -0,0 +1,30 @@
+package com.aish.java8._07primitiveTypeFunctionalInterfaces;
+
+
+/**
+ * IntSupplier Basic Demo
+ */
+
+import java.util.function.IntSupplier;
+
+/**
+ * @author  Aishwarya Jorve
+ */
+public class IntSupplierDemo {
+    public static void main(String[] args) {
+        IntSupplier s=()->(int)(Math.random()*10);
+
+        String otp="";
+        for(int i =0;i<6;i++){
+
+            // method getAsInt which can always takes primitive types as input and return primitive types
+            otp=otp+s.getAsInt();
+        }
+        System.out.println("The 6 digit OTP: "+otp);
+    }
+}
+
+/**
+ * Output
+ * The 6 digit OTP: 959767
+ */
