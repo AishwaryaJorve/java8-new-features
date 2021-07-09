76dd56b7ec94f2ce2c05c54bb916f1d3e45beb28
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2021-07-09 13:06:05 +0530

    For testing

---
+++ src/main/java/com/aish/java8/_07primitiveTypeFunctionalInterfaces/Senario.java
@@ -0,0 +1,32 @@
+package com.aish.java8._07primitiveTypeFunctionalInterfaces;
+
+public class Senario {
+
+    public boolean test(int a[]) {
+        boolean flag=false;
+        if (a.length > 0) {
+            for (int i = 0; i < a.length; i++) {
+                if(a[i]==5){
+                    flag=false;
+                    return false;
+                }
+                else {
+                    System.out.println(a[i]);
+                    flag=true;
+                }
+            }
+        }
+
+        System.out.println("After return false");
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        Senario s = new Senario();
+        s.test(a);
+        System.out.println("In Main method");
+
+    }
+}
