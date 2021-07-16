9b9ebef8d23ac6daaea7413095279938db6ab834
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2021-07-16 12:30:12 +0530

    Basic of UnaryOprator

---
+++ src/main/java/com/aish/java8/_07primitiveTypeFunctionalInterfaces/_05UnaryOpratorBasic.java
@@ -0,0 +1,27 @@
+package com.aish.java8._07primitiveTypeFunctionalInterfaces;
+
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+/**
+ * @author Aishwarya Jorve
+ */
+class TestFunction {
+    public static void main(String[] args) {
+        Function<Integer, Integer> f = i -> i * i;
+        System.out.println(f.apply(5));
+    }
+}
+
+
+/**
+ * If input and output are same type then we should go for UnaryOperator
+ * It is child of Function<T,T>
+ */
+
+public class _05UnaryOpratorBasic {
+    public static void main(String[] args) {
+        UnaryOperator<Integer> f = i -> i * i;
+        System.out.println(f.apply(6));
+    }
+}
