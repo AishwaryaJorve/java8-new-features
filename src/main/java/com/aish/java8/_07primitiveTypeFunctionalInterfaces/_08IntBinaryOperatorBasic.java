d098b7889ac798ec29777c0f5593e2f52b59be04
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2021-07-19 09:17:41 +0530

    Rename

--- src/main/java/com/aish/java8/_07primitiveTypeFunctionalInterfaces/_08IntBinaryOperatorBasic.java
+++ src/main/java/com/aish/java8/_07primitiveTypeFunctionalInterfaces/_08IntBinaryOperatorBasic.java
@@ -11,7 +11,7 @@ import java.util.function.BinaryOperator;
 /**
  * @author Aishwarya Jorve
  */
-public class IntBinaryOperatorBasic {
+public class _08IntBinaryOperatorBasic {
     public static void main(String[] args) {
         BinaryOperator<Integer> b = (i1, i2) -> i1 + i2;
         System.out.println(b.apply(10, 20));
