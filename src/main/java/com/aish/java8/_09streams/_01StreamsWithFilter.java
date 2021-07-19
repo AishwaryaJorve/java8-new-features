520ed0968620438eb1c81a217ed19bab7e9dd509
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2021-07-19 17:12:57 +0530

    Basic of Streams with even no in list

--- src/main/java/com/aish/java8/_09streams/_01StreamsWithFilter.java
+++ src/main/java/com/aish/java8/_09streams/_01StreamsWithFilter.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class _01SListEvenNoWithStreams {
+public class _01StreamsWithFilter {
 
     public static void main(String[] args) {
         ArrayList<Integer> i= new ArrayList<Integer>();
