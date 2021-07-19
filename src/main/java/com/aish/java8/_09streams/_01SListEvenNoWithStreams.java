1487f00fd7f49e45d8c6be611f1e50b8e74290b2
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2021-07-19 16:15:42 +0530

    Basic of Streams with even no in list

---
+++ src/main/java/com/aish/java8/_09streams/_01SListEvenNoWithStreams.java
@@ -0,0 +1,25 @@
+package com.aish.java8._09streams;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class _01SListEvenNoWithStreams {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> i= new ArrayList<Integer>();
+        i.add(1);
+        i.add(10);
+        i.add(14);
+        i.add(11);
+        i.add(79);
+
+        System.out.println(i);
+
+        // Using streams
+        List<Integer> i1=i.stream().filter(l->l%2==0).collect(Collectors.toList());
+        System.out.println(i1);
+
+
+    }
+}
